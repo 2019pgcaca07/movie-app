694791f2ee8069a7008e693d4fd635dc6de423c7
Ranjeet <ranjeetkccc12@gmail.com>
2021-02-12 20:43:31 -0800

    Favouriting Movies

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,11 +7,19 @@ import { data } from "../data";
 
 //action type
 export const ADD_MOVIES = 'ADD_MOVIES';
+export const ADD_FAVOURITE = 'ADD_FAVOURITE';
 
 //action creators
 export function addMovies(movies){
     return {
         type:ADD_MOVIES,
         movies
+    }
+}
+
+export function addFavourite(movie){
+    return {
+        type:ADD_FAVOURITE,
+        movie
     }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
  import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
-import {addMovies  } from "../actions";
+import {addMovies} from "../actions";
 
 class App extends React.Component{
 
@@ -19,6 +19,20 @@ class App extends React.Component{
 
 
   }
+  isMovieFavourite = (movie) =>{
+           
+    const {favourites} = this.props.store.getState();
+    const index = favourites.indexOf(movie);
+
+    if(index !== -1){
+      
+      //found the movie
+      return true;
+    }
+
+    return false;
+
+  }
   render(){
   const {list} = this.props.store.getState();  //{list:[],favourites:[]}
   console.log('RENDER',this.props.store.getState());
@@ -32,7 +46,12 @@ class App extends React.Component{
         </div>
         <div className="list">
           {list.map((movie,index) => (
-            <MovieCard movie={movie} key={'movies-${index}'} />
+            <MovieCard 
+               movie={movie} 
+               key={`movies-${index}`} 
+               dispatch={this.props.store.dispatch}
+               isFavourite = {this.isMovieFavourite(movie)}
+               />
           ))}
         </div>
 
